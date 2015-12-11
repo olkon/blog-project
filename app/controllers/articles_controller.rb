1c08f9bb3b741bd6f28082f64dbf03c725319dc7
olkon <olli.kontkanen@gmail.com>
2015-12-11 14:46:12 +0000

    message/error styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article created successfully!"
+      flash[:success] = "Article created successfully!"
       redirect_to article_path(@article)
     else
       render "new"
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice] = "Article updated successfully!"
+      flash[:success] = "Article updated successfully!"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
     
     @article.destroy
     
-    flash[:notice] = "Article deleted successfully!"
+    flash[:danger] = "Article deleted successfully!"
     redirect_to articles_path
   end
   

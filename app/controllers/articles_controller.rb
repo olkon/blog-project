b423d8ec429565f615411f04870de7c9711571cf
olkon <olli.kontkanen@gmail.com>
2015-12-14 21:52:56 +0000

    controller side user restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
   def create
    
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     
     if @article.save
       flash[:success] = "Article created successfully!"
@@ -56,5 +58,16 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
+    
+    def require_user
+      
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only do that to your own articles!"
+        redirect_to root_path
+      end
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   
   def index
@@ -47,6 +48,13 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def require_same_user
+    if !logged_in? || current_user != @user
+      flash[:danger] = "You can only edit your own account!"
+      redirect_to root_path
+    end
   end
   
 end

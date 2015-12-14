40e1e1063804e43e7dabbc8ac52a50106bb819b3
olkon <olli.kontkanen@gmail.com>
2015-12-14 22:47:36 +0000

    new admin user role, can modify/delete any users' articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -64,7 +64,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only do that to your own articles!"
         redirect_to root_path
       end
